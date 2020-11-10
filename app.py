9c435cfdd4e31da2770c789510f38c826e2dcd00
Juan Jimenez <juans.jimenez2890@gmail.com>
2020-11-09 22:44:08 -0500

    segundo commit

--- app.py
+++ app.py
@@ -732,4 +732,4 @@ def update_table(start, end, clinic, admit_type, heatmap_click, reset_click, *ar
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
